637c234dd2c3554d4c366916c6b9f67faea1abd6
Tanvir Ahammad Chy <tanvirchy.dev@gmail.com>
2022-05-22 13:33:24 +0600

    FEAT: Axios initial setup and API integration has been done

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,6 +1,25 @@
+// Library Import
+
+import { useState, useEffect } from 'react';
+
 // File Import
+import { UserAPI } from '../api';
 
 export const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await UserAPI.getUsers();
+      console.log(response?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <h1>User List</h1>
