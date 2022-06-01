e194582241614982b2ce49764bf17bae45b9692c
Tanvir Ahammad Chy <tanvirchy.dev@gmail.com>
2022-06-01 17:56:04 +0600

    React form validation with formik initial validaiton

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -2,14 +2,14 @@
 import { useState } from 'react';
 import { Form, Button, Modal } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
 
 // File Import
 import { userAction as action } from '../../store/actions/userAction';
+import { useFormik } from 'formik';
 
-const initialState = {
-  id: uuidv4(),
+const initialValues = {
   title: '',
   email: '',
   website: ''
@@ -17,18 +17,42 @@ const initialState = {
 
 export const AddUserModal = ({ show, handleClose }) => {
   const dispatch = useDispatch();
-  const [formData, setFormData] = useState(initialState || {});
+  const [formData, setFormData] = useState(initialValues || {});
 
-  const handleChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
+  // Validation conditions
+  const validate = (values) => {
+    let errors = {};
+
+    if (!values.title) errors.title = 'Title field is required !';
+
+    if (!values.email) {
+      errors.email = 'Email field is required !';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Email format is not valid !';
+    }
+
+    if (!values.website) errors.website = 'Website field is required !';
+
+    return errors;
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(action.addUser(formData));
-    resetForm();
-    hideCreateModal();
-  };
+  // formik main hooks
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => console.log('Form Data: ', values),
+    validate
+  });
+
+  // const handleChange = (event) => {
+  //   setFormData({ ...formData, [event.target.name]: event.target.value });
+  // };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   dispatch(action.addUser(formData));
+  //   resetForm();
+  //   hideCreateModal();
+  // };
 
   const hideCreateModal = () => {
     resetForm();
@@ -36,7 +60,7 @@ export const AddUserModal = ({ show, handleClose }) => {
   };
 
   const resetForm = () => {
-    setFormData(initialState);
+    setFormData(initialValues);
   };
   return (
     <Modal show={show} centered onHide={() => hideCreateModal()} size='lg'>
@@ -44,38 +68,49 @@ export const AddUserModal = ({ show, handleClose }) => {
         <Modal.Title>Add new user</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={formik.handleSubmit}>
           <Form.Group className='mb-3' controlId='form-title'>
             <Form.Label>Title</Form.Label>
             <Form.Control
-              onChange={(event) => handleChange(event)}
+              onChange={formik.handleChange}
+              value={formik.values.title}
+              isInvalid={formik.errors.title}
               name='title'
               type='text'
-              value={formData?.title}
               placeholder='Title'
             />
+            {formik.errors.title && (
+              <Form.Control.Feedback type='invalid'>{formik.errors.title}</Form.Control.Feedback>
+            )}
           </Form.Group>
 
           <Form.Group className='mb-3' controlId='form-email'>
             <Form.Label>Email</Form.Label>
             <Form.Control
-              onChange={(event) => handleChange(event)}
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              isInvalid={formik.errors.email}
               name='email'
               type='email'
-              value={formData?.email}
               placeholder='Enter email'
             />
+
+            <Form.Control.Feedback type='invalid'>{formik.errors.email}</Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className='mb-3' controlId='form-website'>
             <Form.Label>Website</Form.Label>
             <Form.Control
-              onChange={(event) => handleChange(event)}
+              onChange={formik.handleChange}
+              value={formik.values.website}
+              isInvalid={formik.errors.website}
               name='website'
               type='website'
-              value={formData?.website}
               placeholder='Enter website'
             />
+            {formik.errors.website && (
+              <Form.Control.Feedback type='invalid'>{formik.errors.website}</Form.Control.Feedback>
+            )}
           </Form.Group>
           <Button variant='primary' type='submit'>
             Submit
