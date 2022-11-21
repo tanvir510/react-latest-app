479086aeec99d5e0fc12a457c33c4a238d2d27dc
Tanvir Ahammad Chy <55985613+tanvir510@users.noreply.github.com>
2022-11-21 10:29:20 +0600

    React bootstrap initialized on project

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,9 @@
 // File Import
-import { UserList } from '../components/UserList';
 
 function App() {
   return (
     <div className='App'>
       <h1>User List</h1>
-      <UserList />
     </div>
   );
 }
